e8f458bdafb8a2528baee072e72b8c928a2a8f78
Thomas Paradis <tmparadi525@gmail.com>
2020-02-11 19:49:30 -0500

    Day16 part1 solution

---
+++ Day16/day16.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DEBUG_PRINT 0
+
+#define FAILURE -1
+#define SUCCESS 0
+
+#define NUM_PHASES 100
+
+
+void print_as_num(int *sig, int sig_size) {
+  for(int i = 0; i < sig_size; i++) {
+    printf("%d", sig[i]);
+  }
+}
+
+void part1(int *sig_init, int sig_size) {
+  int *sig = (int *) malloc(sizeof(int) * sig_size);
+  int *temp = (int *) malloc(sizeof(int) * sig_size);
+
+  memcpy(sig, sig_init, sizeof(int) * sig_size);
+
+  #if DEBUG_PRINT
+  for(int i = 0; i < sig_size; i++) {
+    printf("sig[%d]=%d\n", i, sig[i]);
+  }
+  #endif
+
+  int pattern[4] = {0, 1, 0, -1};
+
+  int phase = 0;
+
+  while(1) {
+    #if DEBUG_PRINT
+    printf("phase %d: ", phase);
+    print_as_num(sig, sig_size);
+    printf("\n");
+    #endif
+
+    if(phase >= NUM_PHASES) {
+      break;
+    }
+
+    for(int i = 0; i < sig_size; i++) {
+      int sum = 0;
+      for(int j = 0; j < sig_size; j++) {
+        int index = (i + 2 + j)/(i + 1) - 1;
+        sum += sig[j] * pattern[index % 4];
+      }
+      temp[i] = abs(sum % 10);
+    }
+
+    memcpy(sig, temp, sizeof(int) * sig_size);
+    phase++;
+  }
+
+  if(temp) {
+    free(temp);
+    temp = NULL;
+  }
+
+  printf("PART1: ");
+  print_as_num(sig, 8);
+  printf("\n");
+}
+
+void part2(int *modules, int modules_size) {
+  printf("PART2: %d\n", 0);
+}
+
+int parse_file(char *file_name, int **arr, int *arr_size) {
+  FILE *fp;
+  size_t len;
+  char *line = NULL;
+
+
+  if(NULL == (fp = fopen(file_name, "r"))) {
+    printf("failed to open input file %s, exiting\n", file_name);
+    return FAILURE;
+  }
+
+  int *array;
+  int array_size;
+  int line_num = 0;
+
+
+  int n_bytes;
+  while(-1 != (n_bytes = getline(&line, &len, fp))) {
+    if(line_num > 0) {
+       printf("input should only be one line, exiting\n");
+       return FAILURE;
+    }
+    array_size = n_bytes - 1;
+    array = (int *) malloc(sizeof(int) * array_size);
+
+    for(int i = 0; i < array_size; i++) {
+      array[i] = (int)(line[i] - 48);
+    }
+  }
+
+  if(line) {
+    free(line);
+    line = NULL;
+  }
+
+  *arr = array;
+  *arr_size = array_size;
+
+  return SUCCESS;
+}
+
+int main(int argc, char *argv[]) {
+  if(argc != 2) {
+    printf("usage: ./dayXX input.txt\n");
+  } else {
+    if(DEBUG_PRINT == 1) {
+      printf("filename_arg = %s\n", argv[1]);
+    }
+    int *sig;
+    int sig_size;
+    if(SUCCESS == parse_file(argv[1], &sig, &sig_size)) {
+      part1(sig , sig_size);
+      //part2(modules, modules_size);
+      if(sig) {
+        free(sig);
+        sig = NULL;
+      }
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
